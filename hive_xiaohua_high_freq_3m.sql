deb49218d3498d9ed894e5633dbc68b70f3550ee
sinjor <fang_sheng_jie@163.com>
2018-01-04 09:56:55 +0800

    Add files via upload
    新增获取额度

--- hive_xiaohua_high_freq_3m.sql
+++ hive_xiaohua_high_freq_3m.sql
@@ -1,3 +1,25 @@
+/* 1.客户号短期多次获额 */
+drop table if exists cid_apply_derived_3m_hf;
+
+
+create table cid_apply_derived_3m_hf as
+select t1.event_cid,
+       count(t1.event_cid) as cid_apply_3m_hf
+from
+    (select event_cid,
+            collector_tstamp
+     from cid_first_loan) t1
+left outer join
+    (select event_cid,
+            collector_tstamp
+     from behavior_data_source_useful_flatten_2
+     where collector_tstamp is not null
+         and length(collector_tstamp) > 0
+         and event_name = "app_apply") t2 on t1.event_cid = t2.event_cid
+where (t1.collector_tstamp >= t2.collector_tstamp)
+    and (date_sub(t1.collector_tstamp,90) <= substring(t2.collector_tstamp, 1,10))
+group by t1.event_cid;
+
 /* 3.客户号短期多张绑卡 */
 drop table if exists cid_bind_bankcardno_derived_3m_hf;
 
@@ -22,6 +44,44 @@ left outer join
 where (t1.collector_tstamp >= t2.collector_tstamp)
     and (date_sub(t1.collector_tstamp,90) <= substring(t2.collector_tstamp, 1,10))
 group by t1.event_cid;
+
+/* 4.手机号短期多次获额 */
+drop table if exists mobile_apply_derived_3m_hf;
+
+
+create table mobile_apply_derived_3m_hf as
+select t3.event_cid,
+       t3.event_mobile,
+       t4.mobile_apply_3m_hf
+from
+    (select event_cid,
+            event_mobile,
+            collector_tstamp
+     from cid_first_loan) t3
+left outer join
+    (select t1.event_mobile,
+            t1.collector_tstamp,
+            count(t2.event_mobile) as mobile_apply_3m_hf
+     from
+         (select event_cid,
+                 event_mobile,
+                 collector_tstamp
+          from cid_first_loan) t1
+     left outer join
+         (select event_cid,
+                 event_mobile,
+                 collector_tstamp
+          from behavior_data_source_useful_flatten_2
+          where event_mobile is not null
+              and length(event_mobile) >0
+              and collector_tstamp is not null
+              and length(collector_tstamp) > 0
+              and event_name = "app_apply") t2 on t1.event_mobile = t2.event_mobile
+     where (t1.collector_tstamp >= t2.collector_tstamp)
+         and (date_sub(t1.collector_tstamp,90) <= substring(t2.collector_tstamp, 1,10))
+     group by t1.event_mobile,
+              t1.collector_tstamp) t4 on t3.event_mobile = t4.event_mobile
+and t3.collector_tstamp = t4.collector_tstamp;
 
 /* 6.手机号短期多张绑卡 */
 drop table if exists mobile_bind_bankcardno_derived_3m_hf;
@@ -142,15 +202,19 @@ drop table if exists cid_derived_hs_3m;
 
 
 create table cid_derived_hs_3m as
-select t1.event_cid,
+select t0.event_cid,
+       t1.cid_apply_3m_hf,
        t2.cid_bind_bankcardno_3m_hf,
-       t3.mobile_bind_bankcardno_3m_hf,
-       t4.certno_bind_cid_3m_hf,
-       t5.bankcardno_bind_cid_3m_hf
+       t3.mobile_apply_3m_hf,
+       t4.mobile_bind_bankcardno_3m_hf,
+       t5.certno_bind_cid_3m_hf,
+       t6.bankcardno_bind_cid_3m_hf
 from
     (select event_cid
-     from cid_first_loan) t1
-left outer join cid_bind_bankcardno_derived_3m_hf as t2 on t1.event_cid = t2.event_cid
-left outer join mobile_bind_bankcardno_derived_3m_hf as t3 on t1.event_cid = t3.event_cid
-left outer join certno_bind_cid_derived_3m_hf as t4 on t1.event_cid = t4.event_cid
-left outer join bankcardno_bind_cid_derived_3m_hf as t5 on t1.event_cid = t5.event_cid;
+     from cid_first_loan) t0
+left outer join cid_apply_derived_3m_hf as t1 on t0.event_cid = t1.event_cid
+left outer join cid_bind_bankcardno_derived_3m_hf as t2 on t0.event_cid = t2.event_cid
+left outer join mobile_apply_derived_3m_hf as t3 on t0.event_cid = t3.event_cid
+left outer join mobile_bind_bankcardno_derived_3m_hf as t4 on t0.event_cid = t4.event_cid
+left outer join certno_bind_cid_derived_3m_hf as t5 on t0.event_cid = t5.event_cid
+left outer join bankcardno_bind_cid_derived_3m_hf as t6 on t0.event_cid = t6.event_cid;
