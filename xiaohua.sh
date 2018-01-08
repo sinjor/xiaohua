93f8c2ff29d477065a70a6ac137dbfd2271ef7c3
sinjor <fang_sheng_jie@163.com>
2018-01-08 15:19:38 +0800

    增加中文转换表
    1.增加中文转换表
    2.去掉英文转换
    3.增加shell脚本

--- xiaohua.sh
+++ xiaohua.sh
@@ -4,3 +4,5 @@ hive -f hive_xiaohua_am_3m.sql
 hive -f hive_xiaohua_high_freq_1m.sql
 hive -f hive_xiaohua_high_freq_3m.sql
 hive -f hive_xiaohua_abnormal.sql
+hive -f xiaohua_other.sql
+hive -f xiaohua_feature_extract_all.sql
--- xiaohua_other.sql
+++ xiaohua_other.sql
@@ -1,3 +1,116 @@
+-- education_code
+-- position_code
+-- workYears_code
+-- contactsRelationship_code
+-- bankCardName_code
+-- ipCity_code
+-- mobileCity_code
+-- ipProvince_code
+
+drop table if exists education_code;
+
+
+create table education_code as
+select t.event_education,
+       row_number() over(
+                         order by t.event_education) as event_education_code
+from
+    (select distinct event_education as event_education
+     from behavior_data_source_useful_flatten_2
+     where event_education is not null) t;
+
+
+drop table if exists position_code;
+
+
+create table position_code as
+select t.event_position,
+       row_number() over(
+                         order by t.event_position) as event_position_code
+from
+    (select distinct event_position as event_position
+     from behavior_data_source_useful_flatten_2
+     where event_position is not null) t;
+
+
+drop table if exists workYears_code;
+
+
+create table workYears_code as
+select t.event_workyears,
+       row_number() over(
+                         order by t.event_workyears) as event_workyears_code
+from
+    (select distinct event_workyears as event_workyears
+     from behavior_data_source_useful_flatten_2
+     where event_workyears is not null) t;
+
+
+drop table if exists contactsRelationship_code;
+
+
+create table contactsRelationship_code as
+select t.event_contactsrelationship,
+       row_number() over(
+                         order by t.event_contactsrelationship) as event_contactsrelationship_code
+from
+    (select distinct event_contactsrelationship as event_contactsrelationship
+     from behavior_data_source_useful_flatten_2
+     where event_contactsrelationship is not null) t;
+
+
+drop table if exists bankCardName_code;
+
+
+create table bankCardName_code as
+select t.event_bankCardName,
+       row_number() over(
+                         order by t.event_bankcardname) as event_bankcardname_code
+from
+    (select distinct event_bankcardname as event_bankcardname
+     from behavior_data_source_useful_flatten_2
+     where event_bankcardname is not null) t;
+
+
+drop table if exists ipCity_code;
+
+
+create table ipCity_code as
+select t.event_ipCity,
+       row_number() over(
+                         order by t.event_ipcity) as event_ipcity_code
+from
+    (select distinct event_ipcity as event_ipcity
+     from behavior_data_source_useful_flatten_2
+     where event_ipcity is not null) t;
+
+
+drop table if exists mobileCity_code;
+
+
+create table mobileCity_code as
+select t.event_mobileCity,
+       row_number() over(
+                         order by t.event_mobilecity) as event_mobilecity_code
+from
+    (select distinct event_mobilecity as event_mobilecity
+     from behavior_data_source_useful_flatten_2
+     where event_mobilecity is not null) t;
+
+
+drop table if exists ipProvince_code;
+
+
+create table ipProvince_code as
+select t.event_ipprovince,
+       row_number() over(
+                         order by t.event_ipprovince) as event_ipprovince_code
+from
+    (select distinct event_ipprovince as event_ipprovince
+     from behavior_data_source_useful_flatten_2
+     where event_ipprovince is not null) t;
+
+
 drop table if exists cid_first_loan_source_features;
 
 
@@ -37,7 +150,8 @@ select t1.event_cid,
            else 0
        end as event_contactsMobile_flag,
        t6.event_contactsRelationship_code,
-       cast(t1.event_bankCardType as int) as event_bankCardType,
+       t1.event_bankCardType,
+
        cast(t1.event_bankCardCode as int) as event_bankCardCode,
        t7.event_bankCardName_code,
        cast(t1.event_srcChannel as int) as event_srcChannel,
