f5a0d4506667ec76772441ebf663c8c2ceac6212
sinjor <fang_sheng_jie@163.com>
2018-01-12 11:18:25 +0800

    手动复制到xshell
    手动复制到xshell

--- xiaohua.sh
+++ xiaohua.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -x
-table_original=behavior_data_source_useful_flatten_2 #原始数据集
-table_train_src=sinjor_train_data_time              #训练集表格
-table_test_src=sinjor_test_data_time                #测试集表格
-database_train=sinjor_db_train_time                   #训练集数据库
-database_test=sinjor_db_test_time                     #测试集数据库
-table_feature_dest=xiaohua_feature_extract_all   #合并了label之后的特征变量总表
-database_label=default                           #label所在数据库
+
 database_original=default                        #原始数据所在数据库
+database_dest=sinjor_db_time                     #中间数据所在数据库
+database_label=default                           #label所在数据库
+
+table_original=behavior_data_source_useful_flatten_2 #原始数据集
+table_feature_dest=xiaohua_feature_extract_all   #合并了label之后的特征变量总表
 table_label=ml_labels                            #labe数据集
+
+table_train_data=sinjor_train_data_rand
+table_test_data=sinjor_test_data_rand
 
 check_error(){
 exitCode=$?
@@ -17,47 +19,35 @@ if [ $exitCode -ne 0 ];then
          exit $exitCode
 fi
 }
-#从训练集中生成通用的中文英文转换码
-commom_transfrom()
-{
-echo "line:${LINENO}"&&hive -hiveconf source_table=${table_train_src} -hiveconf source_database=${database_train} -f xiaohua_code_from_train.sql\
-&&echo "line:${LINENO}"&&hive -hiveconf source_table=${database_train}.${table_train_src} -hiveconf source_database=${database_test} -f xiaohua_code_from_train.sql
-}
 
+#数据处理
 data_processing()
 {
-  echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_am_1m.sql\
-&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_am_3m.sql\
-&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_high_freq_1m.sql\
-&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_high_freq_3m.sql\
-&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_abnormal.sql\
-&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f xiaohua_other.sql\
-&&echo "line:${LINENO}"&&hive -hiveconf source_database=${database_src} -hiveconf label_table=${table_label} -hiveconf label_database=${database_label} -f xiaohua_feature_extract_all.sql\
-&&echo "line:${LINENO}"&&rm -f ${table_src}.csv\
-&&echo "line:${LINENO}"&&hive -e "set hive.cli.print.header=true;select * from ${database_src}.${table_feature_dest};"|\
-sed -e 's/'${table_feature_dest}'.//g' -e 's/\t/,/g' >> "${table_src}.csv"
+#  echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_am_1m.sql&&\
+#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_am_3m.sql&&\
+#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_high_freq_1m.sql&&\
+#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_high_freq_3m.sql&&\
+#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f hive_xiaohua_abnormal.sql&&\
+#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_src} -hiveconf source_database=${database_src} -f xiaohua_other.sql&&\
+#echo "line:${LINENO}"&&hive -hiveconf source_database=${database_src}\
+#                              -hiveconf label_table=${table_label}\
+#                              -hiveconf label_database=${database_label}\
+#                              -hiveconf train_table=${table_train_data}\
+#                              -hiveconf test_table=${table_test_data}\
+#                              -hiveconf feature_table=${table_feature_dest} -f xiaohua_feature_extract_all.sql&&\
+echo "line:${LINENO}"&&rm -f ${table_train_data}.csv&&\
+echo "line:${LINENO}"&&hive -e "set hive.cli.print.header=true;select * from ${database_src}.${table_train_data};"|\
+sed -e 's/'${table_feature_dest}'.//g' -e 's/\t/,/g' >> "${table_train_data}.csv"&&\
 
+echo "line:${LINENO}"&&rm -f ${table_test_data}.csv&&\
+echo "line:${LINENO}"&&hive -e "set hive.cli.print.header=true;select * from ${database_src}.${table_test_data};"|\
+sed -e 's/'${table_feature_dest}'.//g' -e 's/\t/,/g' >> "${table_test_data}.csv"
+hive -e "set hive.cli.print.header=true;select * from sinjor_train_data_time;" | sed -e 's/sinjor_train_data_time.//g' -e 's/\t/,/g' >>sinjor_train_data_time.csv
 }
 
-#划分训练集和测试集
-hive -hiveconf train_table=${table_train_src} \
-     -hiveconf test_table=${table_test_src} \
-     -hiveconf train_database=${database_train} \
-     -hiveconf test_database=${database_test} \
-     -hiveconf original_table=${database_original}.${table_original} -f divide.sql\
-&&commom_transfrom\
-&&(table_src=${table_train_src};database_src=${database_train};data_processing)\
-&&(table_src=${table_test_src};database_src=${database_test};data_processing)\
-&&echo "data processing success"\
-||check_error
-
-#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_test_src} -hiveconf source_database=${database_test} -f hive_xiaohua_am_1m.sql\
-#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_test_src} -hiveconf source_database=${database_test} -f hive_xiaohua_am_3m.sql\
-#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_test_src} -hiveconf source_database=${database_test} -f hive_xiaohua_high_freq_1m.sql\
-#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_test_src} -hiveconf source_database=${database_test} -f hive_xiaohua_high_freq_3m.sql\
-#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_test_src} -hiveconf source_database=${database_test} -f hive_xiaohua_abnormal.sql\
-#&&echo "line:${LINENO}"&&hive -hiveconf source_table=${table_test_src} -hiveconf source_database=${database_test} -f xiaohua_other.sql\
-#&&echo "line:${LINENO}"&&hive -hiveconf source_database=${database_test} -hiveconf label_table=${table_label} -hiveconf label_database=${database_label} -f xiaohua_feature_extract_all.sql\
-#&&echo "line:${LINENO}"&&rm -f ${table_test_src}.csv\
-#&&echo "line:${LINENO}"&&hive -e "set hive.cli.print.header=true;select * from ${database_test}.${table_feature_dest};"|\
-#sed -e 's/'${table_feature_dest}'.//g' -e 's/\t/,/g' >> "${table_test_src}.csv"
+#数据不易，且删且珍惜
+#hive -e "drop database if exists ${database_dest} cascade;create database ${database_dest};"&&\
+#echo "line:${LINENO}"&&hive -hiveconf source_table=${database_original}.${table_original} -hiveconf source_database=${database_dest} -f xiaohua_code_from_train.sql&&\
+(table_src=${database_original}.${table_original};database_src=${database_dest};data_processing)&&\
+echo "data processing success"||\
+check_error
--- xiaohua_feature_extract_all.sql
+++ xiaohua_feature_extract_all.sql
@@ -1,9 +1,15 @@
+--set source_database=sinjor_db_time;
+--set label_database=default;
+--set label_table=ml_labels;
+--set train_table=sinjor_train_data_time;
+--set test_table=sinjor_test_data_time;
+--set feature_table=xiaohua_feature_extract_all;
 use ${hiveconf:source_database};
 
 
-drop table if exists xiaohua_feature_extract_all;
+drop table if exists ${hiveconf:feature_table};
 
-create table xiaohua_feature_extract_all as
+create table ${hiveconf:feature_table} as
 select row_number() over(order by rand()) as cid,*
 from 
 (select t0.cid as event_cid,
@@ -114,7 +120,8 @@ from
        t6.event_gps_ip_longitude_latitude_flag,
        t6.event_loanAmount,
        t6.event_loanTerm,
-       t6.event_productType
+       t6.event_productType,
+       t7.collector_tstamp
 from
     ${hiveconf:label_database}.${hiveconf:label_table} t0 
 right outer join cid_derived_hs_1m as t1 on t0.cid = t1.event_cid
@@ -122,4 +129,61 @@ left outer join cid_derived_hs_3m as t2 on t1.event_cid = t2.event_cid
 left outer join cid_derived_1m_am_all as t3 on t1.event_cid = t3.event_cid
 left outer join cid_derived_3m_am_all as t4 on t1.event_cid = t4.event_cid
 left outer join cid_derived_abn_all as t5 on t1.event_cid = t5.event_cid
-left outer join cid_first_loan_source_features as t6 on t1.event_cid = t6.event_cid) t;
+left outer join cid_first_loan_source_features as t6 on t1.event_cid = t6.event_cid
+left outer join cid_first_loan as t7 on t1.event_cid = t7.event_cid) t;
+
+
+set source_database=sinjor_db_rand;
+set label_database=default;
+set label_table=ml_labels;
+set train_table=sinjor_train_data_rand;
+set test_table=sinjor_test_data_rand;
+use ${hiveconf:source_database};
+
+drop table if exists ${hiveconf:train_table};
+
+
+create table ${hiveconf:train_table} as
+select *
+from xiaohua_feature_extract_all
+where cid < 70000;
+
+
+drop table if exists ${hiveconf:test_table};
+
+
+create table ${hiveconf:test_table} as
+select *
+from xiaohua_feature_extract_all
+where cid >= 70000;
+
+hive -e "set hive.cli.print.header=true;select * from sinjor_db_time.sinjor_train_data_rand;" | sed -e 's/sinjor_train_data_rand.//g' -e 's/\t/,/g' >>sinjor_train_data_rand.csv
+hive -e "set hive.cli.print.header=true;select * from sinjor_db_time.sinjor_test_data_rand;" | sed -e 's/sinjor_test_data_rand.//g' -e 's/\t/,/g' >>sinjor_test_data_rand.csv
+
+
+
+--set source_database=sinjor_db_time;
+--set label_database=default;
+--set label_table=ml_labels;
+--set train_table=sinjor_train_data_time;
+--set test_table=sinjor_test_data_time;
+--use ${hiveconf:source_database};
+--
+
+--
+--drop table if exists ${hiveconf:train_table};
+--
+--create table ${hiveconf:train_table} as
+--select *
+--from xiaohua_feature_extract_all
+--where collector_tstamp < "2017-10-01 00:00:00";
+--
+--drop table if exists ${hiveconf:test_table};
+--
+--create table ${hiveconf:test_table} as
+--select *
+--from xiaohua_feature_extract_all
+--where collector_tstamp >= "2017-10-01 00:00:00";
+--
+--hive -e "set hive.cli.print.header=true;select * from sinjor_db_time.sinjor_train_data_time;" | sed -e 's/sinjor_train_data_time.//g' -e 's/\t/,/g' >>sinjor_train_data_time.csv
+--hive -e "set hive.cli.print.header=true;select * from sinjor_db_time.sinjor_test_data_time;" | sed -e 's/sinjor_test_data_time.//g' -e 's/\t/,/g' >>sinjor_test_data_time.csv
